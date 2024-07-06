53e2bd4ea9c48e9a48bf5e58097a7ff19879305f
saigowthamchintala <saigowtham30601@gmail.com>
2024-07-07 01:09:21 +0530

    Commit-6-New-Update

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "MERN Stack Developer",
+          "Software Developer",
+          "AWS Cloud Developer",
+          // "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
