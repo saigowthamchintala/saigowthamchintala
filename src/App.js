2d6ccb77a07274f082ff59cf097e7d8bff7298be
saigowthamchintala <saigowtham30601@gmail.com>
2024-12-25 20:02:21 +0530

    Commit-22-Removed-Publications

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/header/header";
 import Main from "./components/main/main";
 import Skills from "./components/skills/skills";
 import Footer from "./components/footer/footer";
-import Publications from "./components/publications/publications";
 import Experience from "./components/experience/experience";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Main />
       <Experience />
       <Skills />
-      <Publications />
       <Footer />
     </>
   );
